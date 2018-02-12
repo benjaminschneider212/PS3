14abc062ff45782d640aa844cdfadda93c34340c
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-02-12 13:01:08 -0600

    Problem 3 and 4
    Put in work for problem 3. setting the S4 class for door

--- ProblemSet3.R
+++ ProblemSet3.R
@@ -30,7 +30,29 @@ PlayGame(door3)
 
 #Problem 3
 
+PlayGame.door<-function(x){
+  UseMethod("PlayGame", x)
+  door<-sample(1:3 ,1) #this draws a random value 1 through 3 that will be compared to the door.
+  if(x==door){ #If the doors match, success will be posted
+    print("Congratulations! You chose the correct door")}
+  if(!x==door){ #If the doors do not match, success will not be posted
+    ("Sorry, you did not pick the correct door.")}
+}
 
+PlayGame.door(door1)
+PlayGame.door(door2)
+PlayGame.door(door3) 
+#"Error: C stack usage  7970296 is too close to the limit" Is coming up. Code runs fine without the second line, but I think I would just be creating a new function and not the generic of the one I created
 
 #Problem 4
+
+setClass(Class="door", #this is the creation of the s4 class door with a numeric value for the door
+         representation=representation(
+           doornumber= "numeric"),
+         prototype=prototype(
+           doornumber=c()
+         )
+)
+
+new("door", doornumber=1) #example of setting an object with class door and the door value of 1
 
