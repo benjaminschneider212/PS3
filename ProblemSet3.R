458bf06e06bb97c585cd1a43995d2be28b6f78fb
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-02-12 13:30:55 -0600

    Number 4 and clean
    Did number four. Changed print to return, updated to use else.

--- ProblemSet3.R
+++ ProblemSet3.R
@@ -19,9 +19,9 @@ class(door3)<- "door"
 PlayGame<-function(x){
   door<-sample(1:3 ,1) #this draws a random value 1 through 3 that will be compared to the door.
   if(x==door){ #If the doors match, success will be posted
-    print("Congratulations! You chose the correct door")}
-  if(!x==door){ #If the doors do not match, success will not be posted
-    ("Sorry, you did not pick the correct door.")}
+    return("Congratulations! You chose the correct door")}
+  else{ #If the doors do not match, success will not be posted
+    return("Sorry, you did not pick the correct door.")}
 }
 #below are some examples of this game, they can be run multiple times. Some will return a congratulations, some will say sorry.
 PlayGame(door1)
@@ -37,9 +37,9 @@ PlayGame.door<-function(x){ #this is to set up the method
 PlayGame.door<-function(x){ #this dictates the method
   door<-sample(1:3 ,1) #this draws a random value 1 through 3 that will be compared to the door.
   if(x==door){ #If the doors match, success will be posted
-    print("Congratulations! You chose the correct door")}
-  if(!x==door){ #If the doors do not match, success will not be posted
-    ("Sorry, you did not pick the correct door.")}
+    return("Congratulations! You chose the correct door")}
+  else{ #If the doors do not match, success will not be posted
+    return("Sorry, you did not pick the correct door.")}
 }
 #these are some examples
 PlayGame.door(door1) 
@@ -51,19 +51,29 @@ PlayGame.door(door3)
 
 setClass(Class="door", #this is the creation of the s4 class door with a numeric value for the door
          representation=representation(
-           doornumber= "numeric"),
+           x= "numeric"),
          prototype=prototype(
-           doornumber=c()
+           x=c()
          )
 )
 
-door1<-new("door", doornumber=1) #example of setting an object with class door and the door value of 1
+door1<-new("door", x=1) #example of setting an object with class door and the door value of 1
+door2<-new("door", x=2)
+door3<-new("door", x=3)
 
 setGeneric("PlayGame",
            function(object="door"){
              standardGeneric("PlayGame")
            })
 
+setMethod("PlayGame", "door",
+          function(object){
+            door<-sample(1:3 ,1)
+            if(object@x==door){#except now we call on values defined by our created object
+              return("Congratulations! You chose the correct door")}
+            else { return("Sorry, you did not pick the correct door.")} 
+          })
 
-
-
+PlayGame(door1)
+PlayGame(door2)
+PlayGame(door3)
